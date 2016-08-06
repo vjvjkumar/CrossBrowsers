019231b319b5f6da4fad5e65386d719cc483c3cc
Vijay <vijay.t.vj@gmail.com>
2016-08-06 23:48:42 +0100

    First Commit

---
+++ src/crossBrowsers/VerifyTitle_CrossBrowsers.java
@@ -0,0 +1,38 @@
+package crossBrowsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class VerifyTitle_CrossBrowsers {
+	
+	WebDriver driver;
+
+	@Parameters("browser")
+	@Test
+	public void verifyPageTitle(String browserName) {
+		
+		if(browserName.equalsIgnoreCase("Firefox"))
+		{
+			driver = new FirefoxDriver();
+		}
+		else if(browserName.equalsIgnoreCase("Chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", "V:\\SeleniumJars\\chromedriver.exe");
+			driver = new ChromeDriver(); 
+		}
+		else if (browserName.equalsIgnoreCase("IE"))
+		{
+			System.setProperty("webdriver.ie.driver", "V:\\SeleniumJars\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.get("http://facebook.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+}
+}	
